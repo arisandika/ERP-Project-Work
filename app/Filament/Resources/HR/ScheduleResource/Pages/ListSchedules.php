72660e5113aa4833eb08573740b3519e34523e31
arisandika <ari.bussiness30@gmail.com>
2025-10-06 18:26:19 +0700

    fix: error on creating models in filament-logger

---
+++ app/Filament/Resources/HR/ScheduleResource/Pages/ListSchedules.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\HR\ScheduleResource\Pages;
+
+use App\Filament\Resources\HR\ScheduleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListSchedules extends ListRecords
+{
+    protected static string $resource = ScheduleResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
