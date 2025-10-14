0b53edf0c9d188c50c5bb3602b41aa752faa59e2
nurul <nurulfauziahh204@gmail.com>
2025-10-14 15:49:27 +0700

    modul customer

--- app/Models/CRM/Customer.php
+++ app/Models/CRM/Customer.php
@@ -3,8 +3,22 @@
 namespace App\Models\CRM;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'nx_customers';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'customer_type',
+    ];
+
+
+
 }
