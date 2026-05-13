d7485dc937ddd28979798fd5b439f5ebc9196284
Nurul Fauziah <nurulfauziahh204@gmail.com>
2026-05-13 17:03:20 +0700

    update serial number tracking n resource

---
+++ storage/framework/views/2ef6323dcc09b41188c1b536dc8e999b.php
@@ -0,0 +1,276 @@
+<?php if (isset($component)) { $__componentOriginal166a02a7c5ef5a9331faf66fa665c256 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal166a02a7c5ef5a9331faf66fa665c256 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament-panels::components.page.index','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament-panels::page'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+    <div class="space-y-6">
+        <!-- Form Pencarian -->
+        <?php if (isset($component)) { $__componentOriginalee08b1367eba38734199cf7829b1d1e9 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalee08b1367eba38734199cf7829b1d1e9 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.section.index','data' => ['heading' => 'Pelacakan Serial Number']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament::section'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['heading' => 'Pelacakan Serial Number']); ?>
+            <div class="space-y-4">
+                <?php echo e($this->form); ?>
+
+
+                <div class="flex gap-2">
+                    <?php if (isset($component)) { $__componentOriginal6330f08526bbb3ce2a0da37da512a11f = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal6330f08526bbb3ce2a0da37da512a11f = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.button.index','data' => ['wire:click' => 'search']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament::button'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['wire:click' => 'search']); ?>
+                        Cari
+                     <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal6330f08526bbb3ce2a0da37da512a11f)): ?>
+<?php $attributes = $__attributesOriginal6330f08526bbb3ce2a0da37da512a11f; ?>
+<?php unset($__attributesOriginal6330f08526bbb3ce2a0da37da512a11f); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal6330f08526bbb3ce2a0da37da512a11f)): ?>
+<?php $component = $__componentOriginal6330f08526bbb3ce2a0da37da512a11f; ?>
+<?php unset($__componentOriginal6330f08526bbb3ce2a0da37da512a11f); ?>
+<?php endif; ?>
+
+                    <?php if (isset($component)) { $__componentOriginal6330f08526bbb3ce2a0da37da512a11f = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal6330f08526bbb3ce2a0da37da512a11f = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.button.index','data' => ['color' => 'gray','wire:click' => 'resetSearch']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament::button'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['color' => 'gray','wire:click' => 'resetSearch']); ?>
+                        Reset
+                     <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal6330f08526bbb3ce2a0da37da512a11f)): ?>
+<?php $attributes = $__attributesOriginal6330f08526bbb3ce2a0da37da512a11f; ?>
+<?php unset($__attributesOriginal6330f08526bbb3ce2a0da37da512a11f); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal6330f08526bbb3ce2a0da37da512a11f)): ?>
+<?php $component = $__componentOriginal6330f08526bbb3ce2a0da37da512a11f; ?>
+<?php unset($__componentOriginal6330f08526bbb3ce2a0da37da512a11f); ?>
+<?php endif; ?>
+                </div>
+            </div>
+         <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $attributes = $__attributesOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $component = $__componentOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__componentOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+
+        <!-- Kondisi: Tidak Ditemukan -->
+        <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php endif; ?><?php if(isset($notFound) && $notFound): ?>
+            <?php if (isset($component)) { $__componentOriginalee08b1367eba38734199cf7829b1d1e9 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalee08b1367eba38734199cf7829b1d1e9 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.section.index','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament::section'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+                <div class="text-sm font-medium text-danger-600">
+                    Serial Number tidak ditemukan.
+                </div>
+             <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $attributes = $__attributesOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $component = $__componentOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__componentOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+        <?php endif; ?><?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if ENDBLOCK]><![endif]--><?php endif; ?>
+
+        <!-- Kondisi: Ditemukan -->
+        <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php endif; ?><?php if(!empty($trackingResult)): ?>
+            <?php if (isset($component)) { $__componentOriginalee08b1367eba38734199cf7829b1d1e9 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalee08b1367eba38734199cf7829b1d1e9 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.section.index','data' => ['heading' => 'Identitas & Status Unit']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament::section'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['heading' => 'Identitas & Status Unit']); ?>
+                <div class="grid grid-cols-1 gap-4 text-sm md:grid-cols-2">
+                    <div>
+                        <div class="text-gray-500">Serial Number</div>
+                        <div class="font-semibold"><?php echo e(data_get($trackingResult, 'serial_number', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Status Saat Ini</div>
+                        <div class="font-semibold"><?php echo e(data_get($trackingResult, 'status', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Nama Product</div>
+                        <div class="font-semibold"><?php echo e(data_get($trackingResult, 'product_name', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Kode Product</div>
+                        <div><?php echo e(data_get($trackingResult, 'product_code', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Gudang Saat Ini</div>
+                        <div><?php echo e(data_get($trackingResult, 'warehouse_name', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Supplier</div>
+                        <div><?php echo e(data_get($trackingResult, 'supplier_name', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Purchase Order</div>
+                        <div><?php echo e(data_get($trackingResult, 'purchase_order_number', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Customer</div>
+                        <div><?php echo e(data_get($trackingResult, 'customer_name', '-')); ?></div>
+                    </div>
+                </div>
+             <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $attributes = $__attributesOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $component = $__componentOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__componentOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+
+            <?php if (isset($component)) { $__componentOriginalee08b1367eba38734199cf7829b1d1e9 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalee08b1367eba38734199cf7829b1d1e9 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.section.index','data' => ['heading' => 'Riwayat Unit']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament::section'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['heading' => 'Riwayat Unit']); ?>
+                <div class="grid grid-cols-1 gap-4 text-sm md:grid-cols-2">
+                    <div>
+                        <div class="text-gray-500">Tanggal Masuk</div>
+                        <div><?php echo e(data_get($trackingResult, 'inbound_date', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Tanggal Keluar</div>
+                        <div><?php echo e(data_get($trackingResult, 'outbound_date', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Garansi Habis</div>
+                        <div><?php echo e(data_get($trackingResult, 'warranty_expired_at', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Status Garansi</div>
+                        <div class="font-semibold"><?php echo e(data_get($trackingResult, 'warranty_status', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Data Dibuat</div>
+                        <div><?php echo e(data_get($trackingResult, 'created_at', '-')); ?></div>
+                    </div>
+
+                    <div>
+                        <div class="text-gray-500">Terakhir Diperbarui</div>
+                        <div><?php echo e(data_get($trackingResult, 'updated_at', '-')); ?></div>
+                    </div>
+                </div>
+             <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $attributes = $__attributesOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $component = $__componentOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__componentOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+
+            <?php if (isset($component)) { $__componentOriginalee08b1367eba38734199cf7829b1d1e9 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalee08b1367eba38734199cf7829b1d1e9 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament::components.section.index','data' => ['heading' => 'Histori Audit Transaksi']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament::section'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['heading' => 'Histori Audit Transaksi']); ?>
+                <div class="space-y-4">
+                    <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php endif; ?><?php $__empty_1 = true; $__currentLoopData = data_get($this, 'transactionHistory', []); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $history): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
+                        <div class="p-4 space-y-2 border rounded-xl">
+                            <div><span class="font-medium">No. Transaksi:</span> <?php echo e(data_get($history, 'transaction_code', '-')); ?></div>
+                            <div><span class="font-medium">Tanggal:</span> <?php echo e(data_get($history, 'transaction_date', '-')); ?></div>
+                            <div><span class="font-medium">Jenis Mutasi:</span> <?php echo e(data_get($history, 'mutation_type', '-')); ?></div>
+                            <div><span class="font-medium">Referensi:</span> <?php echo e(data_get($history, 'reference_number', '-')); ?></div>
+                            <div><span class="font-medium">Gudang:</span> <?php echo e(data_get($history, 'warehouse_name', '-')); ?></div>
+                            <div><span class="font-medium">Diproses Oleh:</span> <?php echo e(data_get($history, 'created_by', '-')); ?></div>
+                            <div><span class="font-medium">Catatan:</span> <?php echo e(data_get($history, 'notes', '-')); ?></div>
+                        </div>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
+                        <div class="text-sm text-gray-500">
+                            Belum ada histori audit untuk serial number ini.
+                        </div>
+                    <?php endif; ?><?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if ENDBLOCK]><![endif]--><?php endif; ?>
+                </div>
+             <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $attributes = $__attributesOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__attributesOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalee08b1367eba38734199cf7829b1d1e9)): ?>
+<?php $component = $__componentOriginalee08b1367eba38734199cf7829b1d1e9; ?>
+<?php unset($__componentOriginalee08b1367eba38734199cf7829b1d1e9); ?>
+<?php endif; ?>
+        <?php endif; ?><?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if ENDBLOCK]><![endif]--><?php endif; ?>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal166a02a7c5ef5a9331faf66fa665c256)): ?>
+<?php $attributes = $__attributesOriginal166a02a7c5ef5a9331faf66fa665c256; ?>
+<?php unset($__attributesOriginal166a02a7c5ef5a9331faf66fa665c256); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal166a02a7c5ef5a9331faf66fa665c256)): ?>
+<?php $component = $__componentOriginal166a02a7c5ef5a9331faf66fa665c256; ?>
+<?php unset($__componentOriginal166a02a7c5ef5a9331faf66fa665c256); ?>
+<?php endif; ?>
+<?php /**PATH D:\Nurul Fauziah\Project Work\Nexicon\ERP-Project-Work\resources\views/filament/pages/inventory/serial-number-tracking.blade.php ENDPATH**/ ?>
