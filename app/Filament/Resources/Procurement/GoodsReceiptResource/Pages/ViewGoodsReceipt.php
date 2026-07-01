bda7cbecdaffdf4600fdf44c6917fd0d2dc0e584
Nurul Fauziah <nurulfauziahh204@gmail.com>
2026-07-01 12:23:58 +0700

    fix: detail item good receipt

--- app/Filament/Resources/Procurement/GoodsReceiptResource/Pages/ViewGoodsReceipt.php
+++ app/Filament/Resources/Procurement/GoodsReceiptResource/Pages/ViewGoodsReceipt.php
@@ -3,10 +3,24 @@
 namespace App\Filament\Resources\Procurement\GoodsReceiptResource\Pages;
 
 use App\Filament\Resources\Procurement\GoodsReceiptResource;
+use App\Models\Procurement\GoodsReceipt;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewGoodsReceipt extends ViewRecord
 {
     protected static string $resource = GoodsReceiptResource::class;
+
+    protected function resolveRecord(string|int $key): GoodsReceipt
+    {
+        return static::getResource()::getModel()::query()
+            ->with([
+                'items.product',
+                'purchaseOrder',
+                'supplier',
+                'warehouse',
+                'receiver',
+            ])
+            ->findOrFail($key);
+    }
 }
