9d79f1622623e46f299c238cbadb0b4f406ae518
Nurul Fauziah <nurulfauziahh204@gmail.com>
2026-08-02 14:30:47 +0700

    refactor: simplify dashboard-hr blade template
    Remove the conditional check for header widgets in the HR dashboard
    to streamline the widget rendering process.
    
    Note: Compiled view files in `storage/framework/views/` were
    unintentionally included in the changes.

--- resources/views/filament/pages/hr/dashboard-hr.blade.php
+++ resources/views/filament/pages/hr/dashboard-hr.blade.php
@@ -1,11 +1,4 @@
 <x-filament-panels::page>
-    @if (count($this->getHeaderWidgets()))
-        <x-filament-widgets::widgets
-            :widgets="$this->getHeaderWidgets()"
-            :columns="$this->getHeaderWidgetsColumns()"
-        />
-    @endif
-
     <x-filament-widgets::widgets
         :widgets="$this->getWidgets()"
         :columns="$this->getColumns()"
