5394a5082a92432b33c067e0f3d535de1060a456
arisandika <ari.bussiness30@gmail.com>
2026-09-02 21:58:20 +0700

    feat: implement package stock validation service and enhance form validation for package creation and editing

--- app/Filament/Resources/Inventory/PackageResource/Pages/CreatePackage.php
+++ app/Filament/Resources/Inventory/PackageResource/Pages/CreatePackage.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\Inventory\PackageResource\Pages;
 
 use App\Filament\Resources\Inventory\PackageResource;
+use App\Services\Inventory\PackageStockValidationService;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Validation\ValidationException;
 
 class CreatePackage extends CreateRecord
 {
@@ -13,6 +16,33 @@ class CreatePackage extends CreateRecord
     public function getTitle(): string
     {
         return 'Tambah Paket';
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        try {
+            $data['items'] = app(PackageStockValidationService::class)
+                ->normalizeAndValidate($data['items'] ?? []);
+        } catch (ValidationException $exception) {
+            Notification::make()
+                ->danger()
+                ->title('Stock product tidak mencukupi')
+                ->body(implode(' ', $exception->validator->errors()->all()))
+                ->send();
+
+            throw $exception;
+        }
+
+        return $data;
+    }
+
+    protected function onValidationError(ValidationException $exception): void
+    {
+        Notification::make()
+            ->danger()
+            ->title('Package gagal disimpan')
+            ->body(implode(' ', $exception->validator->errors()->all()))
+            ->send();
     }
 
     protected function getRedirectUrl(): string
--- app/Filament/Resources/Inventory/PackageResource/Pages/EditPackage.php
+++ app/Filament/Resources/Inventory/PackageResource/Pages/EditPackage.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\Inventory\PackageResource\Pages;
 
 use App\Filament\Resources\Inventory\PackageResource;
+use App\Services\Inventory\PackageStockValidationService;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Validation\ValidationException;
 
 class EditPackage extends EditRecord
 {
@@ -23,6 +26,33 @@ class EditPackage extends EditRecord
     public function getTitle(): string
     {
         return 'Edit Paket';
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        try {
+            $data['items'] = app(PackageStockValidationService::class)
+                ->normalizeAndValidate($data['items'] ?? []);
+        } catch (ValidationException $exception) {
+            Notification::make()
+                ->danger()
+                ->title('Stock product tidak mencukupi')
+                ->body(implode(' ', $exception->validator->errors()->all()))
+                ->send();
+
+            throw $exception;
+        }
+
+        return $data;
+    }
+
+    protected function onValidationError(ValidationException $exception): void
+    {
+        Notification::make()
+            ->danger()
+            ->title('Package gagal disimpan')
+            ->body(implode(' ', $exception->validator->errors()->all()))
+            ->send();
     }
 
     protected function getRedirectUrl(): string
